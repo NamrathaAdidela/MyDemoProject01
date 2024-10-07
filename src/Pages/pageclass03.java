fd04536d30ca706c5f35bc49904743947b14e834
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-07 10:54:49 +0100

    Commiting the changes 01

---
+++ src/Pages/pageclass03.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class pageclass03 {
+	public static void main(String s) {
+		System.out.println("Page class 3");
+	}
+}
---
+++ src/tests/test03.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class test03 {
+	public void test2() {
+		System.out.println("test class 3");
+	}
+}
