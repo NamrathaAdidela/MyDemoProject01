a897579e5661da548bf2c90d12a2215bc74fb9e7
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-08 10:46:21 +0100

    Test to delete a branch at remote side

---
+++ src/Pages/chldbrnchdemo01.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class chldbrnchdemo01 {
+	public static void main(String s) {
+		System.out.println("child branch demo Page class 01");
+		System.out.println("child branch demo Page class 01.01");
+	}
+}
---
+++ src/tests/cldbrnchdemotest01.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class cldbrnchdemotest01 {
+	public void test1() {
+		System.out.println("child branch demo test class 01");
+	}
+}
