9fb0a1d28ed523fbf4708d1529c7283056fd6793
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-09 18:55:58 +0100

    Subrnch merge demo commit

---
+++ src/Pages/mergedemopage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class mergedemopage {
+	public static void main(String s) {
+		System.out.println("Merge demo Page class 1");
+		System.out.println("Merge demo Page class 1.01");
+		System.out.println("Merge demo Page class 1.02");
+	}
+	
+}
