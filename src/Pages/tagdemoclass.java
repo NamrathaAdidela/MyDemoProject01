55f07826ec1605a0b974ef055bdff3292ff1675c
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-09 17:02:35 +0100

    tag delete demo

--- src/Pages/tagdemoclass.java
+++ src/Pages/tagdemoclass.java
@@ -4,5 +4,6 @@ public class tagdemoclass {
 	public static void main(String s) {
 		System.out.println("tag demo Page class 1");
 		System.out.println("tag demo Page class 1.01");
+		System.out.println("tag demo Page class 1.02");
 	}
 }
