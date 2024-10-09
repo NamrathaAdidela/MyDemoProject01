2ff17eee0c64b984ba913f7b888c92afeeebb93e
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-09 16:47:19 +0100

    tage demo 2

--- src/Pages/tagdemoclass.java
+++ src/Pages/tagdemoclass.java
@@ -3,5 +3,6 @@ package Pages;
 public class tagdemoclass {
 	public static void main(String s) {
 		System.out.println("tag demo Page class 1");
+		System.out.println("tag demo Page class 1.01");
 	}
 }
