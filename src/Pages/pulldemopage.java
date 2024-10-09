7b4190184a6eecedd08fab43e612da50f7217529
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-09 14:22:02 +0100

    Checking pull request

---
+++ src/Pages/pulldemopage.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class pulldemopage {
+	public static void main(String s) {
+		System.out.println("pull demo Page class 1");
+	}
+}
---
+++ src/tests/pulldemotest.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class pulldemotest {
+	public void pulltest1() {
+		System.out.println("pull demo test class 1");
+	}
+}
