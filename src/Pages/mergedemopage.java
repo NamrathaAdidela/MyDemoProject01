3d308ec4ffba6ae50f68019cdcce282d21057c3b
Namratha Adidela <nreddyadidela@gmail.com>
2024-10-10 13:44:29 +0100

    Checking Stash command in git

--- src/Pages/mergedemopage.java
+++ src/Pages/mergedemopage.java
@@ -5,9 +5,15 @@ public class mergedemopage {
 		System.out.println("Merge demo Page class 1");
 		System.out.println("Merge demo Page class 1.01");
 		System.out.println("Merge demo Page class 1.02");
+//<<<<<<< Updated upstream
 		String str = "Stash";
 		String str1 = "Checking";
 		System.out.println(str+str1);
+//=======
+		int i = 20;
+		System.out.println("age is "+ i);
+//>>>>>>> Stashed changes
+		System.out.println("Explored stash command in git");
 		
 	}
 	
