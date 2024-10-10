7e23add69c15d2638770c985753b15f350a155d6
Namratha Adidela <145983546+NamrathaAdidela@users.noreply.github.com>
2024-10-10 12:33:42 +0100

    Update mergedemopage.java

--- src/Pages/mergedemopage.java
+++ src/Pages/mergedemopage.java
@@ -5,6 +5,10 @@ public class mergedemopage {
 		System.out.println("Merge demo Page class 1");
 		System.out.println("Merge demo Page class 1.01");
 		System.out.println("Merge demo Page class 1.02");
+		String str = "Stash";
+		String str1 = "Checking");
+		System.out.println(str+str1);
+		
 	}
 	
 }
